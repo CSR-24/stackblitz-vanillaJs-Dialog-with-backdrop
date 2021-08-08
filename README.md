20fe2607dc77026d8c807a981c6876ac0835a891
CSR-24 <chetan.raikwal@yahoo.com>
2021-08-08 10:31:53 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-hwujuj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-hwujuj)
---
+++ script.js
@@ -0,0 +1,35 @@
+function confirmBtnClickHandler() {
+  displayBackdrop(false);
+  displayDialog(false);
+}
+function cancelBtnClickHandler() {
+  displayBackdrop(false);
+  displayDialog(false);
+}
+
+const displayBackdrop = display => {
+  const backdrop = document.getElementById('backdropMain');
+  if (backdrop) {
+    if (display) {
+      backdrop.classList.remove('hide');
+    } else {
+      backdrop.classList.add('hide');
+    }
+  }
+};
+
+const displayDialog = display => {
+  const dialog = document.getElementById('dialogContainer');
+  if (dialog) {
+    if (display) {
+      dialog.classList.remove('hide');
+    } else {
+      dialog.classList.add('hide');
+    }
+  }
+};
+
+function btnClickHandler() {
+  displayBackdrop(true);
+  displayDialog(true);
+}
